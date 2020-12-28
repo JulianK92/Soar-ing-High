82e4aae94927d230159ac057ae561f61ee840345
JulianK <julian.k92@outlook.com>
2020-12-28 18:04:01 +0100

    start_project3.sh is now the bash to start! added tests
    added mapfiles from some tests

---
+++ project3/launch/start_project3.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+# make bash files executable after download
+chmod a+rwx "rand.sh"
+chmod a+rwx "rand_Y.sh"
+
+echo "$1"
+# starte erstes launchfile mit explore map
+gnome-terminal -e "roslaunch project3 myfile.launch my_x:=$(./rand.sh) my_y:=$(./rand.sh) my_Y:=$(./rand_Y.sh)"
+sleep 5
+
+# create param for explore node finished or not
+rosparam set exploring true
+sleep 1
+
+gnome-terminal -e "roslaunch project3 explore.launch" 
+
+var=$(rosparam get exploring)
+echo "exploring is set to "
+echo $var
+while $var;
+do
+sleep 10
+rosparam get exploring
+var=$(rosparam get exploring)
+echo "In der Warteschleife!"
+done
+echo "exploring is set to "
+echo $var
+
+echo "Endlich an der Reihe"
+# Sobald explore node fertig dann speicher map
+mapname="mymap$1"
+echo "$mapname"
+rosrun map_server map_saver -f mymap #$mapname
+sleep 5
+echo "becoming a serial killer"
+
+killall -9 gzserver
+killall -9 gzclient
+killall -9 roscore
+killall -9 rosmaster
+killall rviz
+
+echo "blood everywhere"
+sleep 10
+
+echo "one last launch"
+# danach launcheeeeee zweites launchfile mit gespeicherter map
+gnome-terminal -e "roslaunch project3 launch_mapped_maze.launch my_x:=$(./rand.sh) my_y:=$(./rand.sh) my_Y:=$(./rand_Y.sh)" &
+
+sleep 15 
+
+# publishe das goal am exit
+rostopic pub /move_base_simple/goal geometry_msgs/PoseStamped '{header: {stamp: now, frame_id: "map"}, pose: {position: {x: 0.5, y: -6.0, z: 0.0}, orientation: {w: 1.0}}}'
